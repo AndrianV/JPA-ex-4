5afdbb163b1d7cd78a5b3aa8c04636583f8055ac
Andrian Vangelov <andrian.vangelov@gmail.com>
2017-08-26 17:16:02 +0200

    Excercise finished

--- src/main/java/entity/Tester.java
+++ src/main/java/entity/Tester.java
@@ -9,22 +9,67 @@ public class Tester {
     private static final EntityManager em = emf.createEntityManager();
     
     public static void main(String[] args) {
+              
+        
+        createStudents();
+        System.out.println("*******************");
         System.out.println(getAllStudents());
         System.out.println("*******************");
-        System.out.println(getAllStudentsAnders());
+        System.out.println(getAllStudentsByFirstName("Anders"));
+        System.out.println("*******************");
+        assignSemester(getAllStudentsByFirstName("1").get(0));
+        System.out.println(findAllStudentsByLastName("and"));
+        System.out.println("*******************");
+        System.out.println("Total sum for CLcos-v14e is: " + totalSumForSemester("CLcos-v14e"));
+        System.out.println("*******************");
+        System.out.println("Total sum for Students with Semester is: " + sumOfStudentsWithSemester());
+        System.out.println("*******************");
+        System.out.println("Teacher with most classes is :" + findBusyTeacher().getFirstname());
+        System.out.println("*******************");
+        
         
         
     }
     
     public static List<Student> getAllStudents() {
-        Query query = em.createQuery("SELECT s FROM Student s");
-        return (List<Student>) query.getResultList();
+        return em.createNamedQuery("Student.findAll", Student.class).getResultList();
     }
     
-    public static List<Student> getAllStudentsAnders() {
-        Query query = em.createQuery("SELECT s FROM Student s WHERE s.firstname = 'Anders'");
-        return (List<Student>) query.getResultList();
+    public static List<Student> getAllStudentsByFirstName(String name) {
+        return em.createNamedQuery("Student.findByFirstname", Student.class).setParameter("firstname", name).getResultList();
     } 
     
+    public static void createStudents() {
+        em.getTransaction().begin();
+        for(int i = 0; i<10; i++) {
+            Student s = new Student();
+            s.setFirstname(Integer.toString(i));
+            em.persist(s);
+        }
+        em.getTransaction().commit();
+    }
     
+    public static void assignSemester(Student s) {
+        s.setSemester(em.find(Semester.class, 1l));
+        em.getTransaction().begin();
+        em.persist(s);
+        em.getTransaction().commit();
+    }
+    
+    public static List<Student> findAllStudentsByLastName(String name) {
+        return em.createNamedQuery("Student.findByLastname", Student.class).setParameter("lastname", name).getResultList();
+    }
+    
+    public static int totalSumForSemester(String sem) {
+        return em.createQuery("SELECT s FROM Student s WHERE s.semester.name = :name", Student.class).setParameter("name", sem).getResultList().size();
+    }
+    
+    public static int sumOfStudentsWithSemester() {
+        return em.createQuery("SELECT s FROM Student s WHERE s.semester IS NOT NULL", Student.class).getResultList().size();
+    }
+    
+    public static Teacher findBusyTeacher() {
+        Long tID = (Long) em.createNativeQuery("SELECT teachers_id FROM teacher_semester GROUP BY teachers_id ORDER BY COUNT(*) DESC LIMIT 1").getSingleResult();
+        return em.createQuery("SELECT t FROM Teacher t WHERE t.id = ?1", Teacher.class).setParameter(1, tID).getSingleResult();
+    }
 }
